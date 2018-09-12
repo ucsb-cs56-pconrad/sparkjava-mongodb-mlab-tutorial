c24eeb6ba46bc95fc2d149d300e9d019a5acbdf5
Maga Kim <maga@umail.ucsb.edu>
2018-09-11 23:46:17 -0700

    Removed starter code from main class

--- src/main/java/edu/ucsb/cs56/GauchoGains/MongoGains.java
+++ src/main/java/edu/ucsb/cs56/GauchoGains/MongoGains.java
@@ -102,13 +102,9 @@ public class MongoGains {
 				});
 		
 		String uriString = mongoDBUri(envVars);
-			
-        ArrayList<String> dbText = initDatabase(uriString);
-        final String displayText = makeString(dbText);
         port(getHerokuAssignedPort());
-		spark.Spark.get("/", (req, res) -> displayText);
-		System.out.println("Spark is listening for connections...");
-	}
+
+    }
 	
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
@@ -118,107 +114,12 @@ public class MongoGains {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
-    static ArrayList<String> initDatabase(String uriString) {
-        ArrayList<String> dbQuery = new ArrayList<>();
-        try {
-            dbQuery = createDocument(uriString);
-        } catch (UnknownHostException e) {
-            System.out.println("Unknown Host thrown");
-        }
-        return dbQuery;
-    }
-
     static String makeString(ArrayList<String> text) {
         String resultString = "";
         for (String s: text) {
             resultString += "<b> " + s + "</b><br/>";
         }
         return resultString;
-    }
-
-    public static ArrayList<String> createDocument(String uriString) throws UnknownHostException {
-		
-        // Create seed data
-        
-        List<Document> seedData = new ArrayList<Document>();
-		
-        seedData.add(new Document("decade", "1970s")
-					 .append("artist", "Debby Boone")
-					 .append("song", "You Light Up My Life")
-					 .append("weeksAtOne", 10)
-					 );
-		
-        seedData.add(new Document("decade", "1980s")
-					 .append("artist", "Olivia Newton-John")
-					 .append("song", "Physical")
-					 .append("weeksAtOne", 10)
-					 );
-		
-        seedData.add(new Document("decade", "1990s")
-					 .append("artist", "Mariah Carey")
-					 .append("song", "One Sweet Day")
-					 .append("weeksAtOne", 16)
-					 );
-		
-        MongoClientURI uri  = new MongoClientURI(uriString); 
-        MongoClient client = new MongoClient(uri);
-        MongoDatabase db = client.getDatabase(uri.getDatabase());
-        
-        /*
-         * First we'll add a few songs. Nothing is required to create the
-         * songs collection; it is created automatically when we insert.
-         */
-        
-        MongoCollection<Document> songs = db.getCollection("songs");
-        
-        /*
-         *  Dropping the table right before the new setup. 
-         *  This way you can see the changes on mLab, wihtout duplicates.
-         */
-		
-        songs.drop();
-		
-        // Note that the insert method can take either an array or a document.
-        
-        songs.insertMany(seedData);
-		
-        /*
-         * Then we need to give Boyz II Men credit for their contribution to
-         * the hit "One Sweet Day".
-         */
-		
-        Document updateQuery = new Document("song", "One Sweet Day");
-        songs.updateOne(updateQuery, new Document("$set", new Document("artist", "Mariah Carey ft. Boyz II Men")));
-        
-        /*
-         * Finally we run a query which returns all the hits that spent 10 
-         * or more weeks at number 1.
-         */
-		
-        Document findQuery = new Document("weeksAtOne", new Document("$gte",10));
-        Document orderBy = new Document("decade", 1);
-		
-        MongoCursor<Document> cursor = songs.find(findQuery).sort(orderBy).iterator();
-        
-        ArrayList<String> prettyQuery = new ArrayList<>();
-        
-        try {
-            while (cursor.hasNext()) {
-                Document doc = cursor.next();
-                String currSong = "In the " + doc.get("decade") + ", " + doc.get("song") + 
-                    " by " + doc.get("artist") + " topped the charts for " + 
-                    doc.get("weeksAtOne") + " straight weeks.";
-                prettyQuery.add(currSong);
-            }
-        } finally {
-            cursor.close();
-        }
-        
-        // Only close the connection when your app is terminating
-        
-        client.close();
-		
-        return prettyQuery;
     }
 
 }
